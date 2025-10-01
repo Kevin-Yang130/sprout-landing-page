ee75b0a43d017d8cac14ea688d8b22d80baa2569
Kevin Yang-Li <yanglikevin@gmail.com>
2025-10-01 15:25:11 -0400

    Finalized the logo and favicon of Sprout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,10 @@ const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/logo.png" alt="logo" width={74} height={29} />
+        <Image src="/sprout.svg" alt="logo" width={200} height={100} />
       </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex"></ul>
     </nav>
   );
 };
