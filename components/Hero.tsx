70884be21761d4044d31fafc43588eeba31695a0
Kevin Yang-Li <yanglikevin@gmail.com>
2025-10-15 15:33:56 -0400

    Created the Hero and attached iphone images as well as text description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,26 +4,17 @@ import Button from "@/components/Button";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-28 xl:flex-row border-2 border-red-500">
-      <div className="hero-map" />
-
+    <section className="max-container padding-container flex flex-col gap-8 py-10 pb-32 lg:py-28 xl:flex-row xl:gap-0 ">
       {/* LEFT */}
 
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <Image
-          src="/camp.svg"
-          alt="camp"
-          width={50}
-          height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
-        />
+      <div className="relative z-20 flex flex-1 gap-2 flex-col xl:w-1/2  justify-center items-center">
         <h1 className="bold-52 lg:bold-88">Think less, save more.</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        <p className="regular-16 py-6 text-gray-30 xl:max-w-[520px]">
           Take control of your finances with ease. Our app helps you track
           spending, set goals, and save effortlessly all in one place. Simple,
           smart, and designed to help you make every dollar count.
         </p>
-        <div className="flex flex-col w-full gap-3 sm:flex-row">
+        <div className="flex flex-col w-full gap-3 sm:flex-row justify-center">
           <Button type="button" title="Download App" variant="btn_green" />
           <Button
             type="button"
@@ -32,6 +23,17 @@ const Hero = () => {
             variant="btn_white_text"
           />
         </div>
+      </div>
+
+      {/* RIGHT: image next to the headline on large screens */}
+      <div className="relative z-10 flex-1 xl:w-1/2 w-full h-[260px] md:h-[360px] lg:h-[550px] rounded-5xl overflow-hidden  xl:-translate-x-25">
+        <Image
+          src="/t5.png"
+          alt="Sprout app preview"
+          fill
+          className="object-cover object-center scale-110 md:scale-102" // <- zoom
+          priority
+        />
       </div>
     </section>
   );
